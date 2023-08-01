57813d8779e0a088290da4561a13d75ee4c301ce
Ivan63-coder <120375849+Ivan63-coder@users.noreply.github.com>
2023-08-01 20:41:39 +0400

    Add files via upload

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,14 +3,14 @@ from rest_framework import routers
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
-router = routers.DefaultRouter()
-router.register(r'^posts/(?P<post_id>\d+)/comments',
+v1_router = routers.DefaultRouter()
+v1_router.register(r'^posts/(?P<post_id>\d+)/comments',
                 CommentViewSet,
                 basename='comments')
-router.register('groups', GroupViewSet, basename='groups')
-router.register('posts', PostViewSet, basename='posts')
+v1_router.register('groups', GroupViewSet, basename='groups')
+v1_router.register('posts', PostViewSet, basename='posts')
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
